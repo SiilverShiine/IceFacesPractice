e0cdece5b332e48c922a612c8dbd88f8c99b9d40
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2024-07-28 00:26:39 -0500

    test: base index page to test ICEfaces

--- src/main/java/scopePractice/ApplicantController.java
+++ src/main/java/scopePractice/ApplicantController.java
@@ -2,8 +2,10 @@ package scopePractice;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ApplicationScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 
+@Named
 @ManagedBean(name = "applicantController")
 @ApplicationScoped
 public class ApplicantController implements Serializable {
